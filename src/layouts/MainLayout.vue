b465a4068d9429da4044643aff9edbc13a2faccd
nashiusagi <ritotech0226@gmail.com>
2023-04-15 14:34:27 +0900

    [update] デザインの調整
    背景色の変更とGlassCardのパディング調整

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
     <div
-        class="pl-8 py-8 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-max"
+        class="pl-8 py-8 bg-gradient-to-r from-stone-200 from-10% via-sky-100 via-50% to-emerald-500 to-90% h-max"
     >
         <div class="w-full overflow-hidden">
             <slot />
