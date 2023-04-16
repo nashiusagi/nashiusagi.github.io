0b169589fdf8c06e90babeae5257b8ac4a185e19
nashiusagi <ritotech0226@gmail.com>
2023-04-16 11:41:02 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -100,7 +100,7 @@
                         </h3>
                         <ul class="list-disc ml-8 mb-4">
                             <li>
-                                Ryogo Ito, Mitsuhiro Uchida, and Suguru Saito.
+                                <a class="font-bold">Ryogo Ito</a>, Mitsuhiro Uchida, and Suguru Saito.
                                 Decision of Line Structure beyond Junctions
                                 Using U-Net-Based CNN for Line Drawing
                                 Rendering. SIGGRAPH Asia 2021 Posters. 2021.
@@ -111,7 +111,7 @@
                         </h3>
                         <ul class="list-disc ml-8 mb-4">
                             <li>
-                                伊藤 諒悟, 齋藤 豪, 森合 遼.
+                                <a class="font-bold">伊藤 諒悟</a>, 齋藤 豪, 森合 遼.
                                 疑似相対深度推定によるイラストへの重なりを考慮した差し込み.
                                 VC+VCC2022, 2022.
                             </li>
@@ -121,26 +121,26 @@
                         </h3>
                         <ul class="list-disc ml-8 mb-4">
                             <li>
-                                伊藤 諒悟, 齋藤 豪, 内田 光洋.
+                                <a class="font-bold">伊藤 諒悟</a>, 齋藤 豪, 内田 光洋.
                                 U-NetベースのCNNを用いた結節点における線分接続判定.
-                                VC+VCC2021, 2021.
+                                VC+VCC2021ポスター. 2021.
                             </li>
                             <li>
-                                伊藤 諒悟, 齋藤 豪, 森合 遼.
+                                <a class="font-bold">伊藤 諒悟</a>, 齋藤 豪, 森合 遼.
                                 線画像における相対深度グラフ作成手法とそれを用いた線画像の差し込み編集法.
-                                CGVI第189回研究発表会, 2023.
+                                CGVI第189回研究発表会. 2023.
                             </li>
                             <li>
-                                伊藤 諒悟, 齋藤 豪, 森合 遼.
+                                <a class="font-bold">伊藤 諒悟</a>, 齋藤 豪, 森合 遼.
                                 相対的深度情報グラフを用いた線画像の差し込み編集法.
-                                情報処理学会第85回全国大会, 2023.
+                                情報処理学会第85回全国大会. 2023.
                                 （学生奨励賞受賞）
                             </li>
                         </ul>
                         <h3 class="text-xl font-bold">Journal Paper</h3>
                         <ul class="list-disc ml-8 mb-4">
                             <li>
-                                Dingkun Yan, Ryogo Ito, Ryo Moriai, and Suguru Saito.  Two-step Training: Adjustable Sketch Colorization via Reference Image and Text Tag. Computer Graphics Forum.  TBD.  
+                                Dingkun Yan, <a class="font-bold">Ryogo Ito</a>, Ryo Moriai, and Suguru Saito.  Two-step Training: Adjustable Sketch Colorization via Reference Image and Text Tag. Computer Graphics Forum.  TBD.  
                             </li>
                         </ul>
                     </div>
