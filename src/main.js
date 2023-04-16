fd170f7cde7d1b70b566f9ed35e3a8fd74b28d55
nashiusagi <ritotech0226@gmail.com>
2023-04-16 13:15:15 +0900

    [update] introduce vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import './index.css'
+import "./index.css";
+import router from "./router";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 library.add(faTwitter);
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App)
+    .use(router)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MainPage from "@/pages/MainPage.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "MainPage",
+        component: MainPage,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
