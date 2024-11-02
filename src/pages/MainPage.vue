a1c5bb1dd84d8ac2f477723063cc6b344cb0b3aa
nashiusagi <ritotech0226@gmail.com>
2024-11-02 17:17:27 +0900

    [add] title box

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MainLayout from "@/layouts/MainLayout.vue";
+import TitleBox from "@/components/atoms/TitleBox.vue";
 import ProfileCard from "@/components/organisms/ProfileCard.vue";
 import HistoryCard from "@/components/organisms/HistoryCard.vue";
 import SkillCard from "@/components/organisms/SkillCard.vue";
@@ -8,11 +9,7 @@ import CommitmentCard from "@/components/organisms/CommitmentCard.vue";
 
 <template>
   <MainLayout>
-    <div class="bg-white px-2 w-64">
-      <h1 class="text-4xl text-sky-400 font-bold mb-6">
-        <a class="font-zenKurenaido" href="https://nashiusagi.github.io">NashiUsa Web</a>
-      </h1>
-    </div>
+    <TitleBox />
     <div class="my-2">Hello there! I'm NashiUsagi!</div>
     <div class="text-gray-900 subpixel-antialiased">
       <ProfileCard />
