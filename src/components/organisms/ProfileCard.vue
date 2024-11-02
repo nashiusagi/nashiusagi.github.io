443ce9873f65ebd69958f8d3217a169b9fdc0f1a
nashiusagi <ritotech0226@gmail.com>
2024-11-02 17:19:23 +0900

    [fix] フォーマットチェック

--- src/components/organisms/ProfileCard.vue
+++ src/components/organisms/ProfileCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import GlassCard from '@/components/molecules/GlassCard.vue';
+import GlassCard from "@/components/molecules/GlassCard.vue";
 import GithubButton from "vue-github-button";
 import TwitterButton from "@/components/molecules/TwitterButton.vue";
 </script>
