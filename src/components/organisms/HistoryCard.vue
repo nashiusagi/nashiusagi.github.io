600ac6a5500eba092894a771d40816b6c0eaec92
nashiusagi <ritotech0226@gmail.com>
2024-11-02 17:06:03 +0900

    [update] molecule, organismsを切る

---
+++ src/components/organisms/HistoryCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import GlassCard from "@/components/molecules/GlassCard.vue";
+</script>
+
+<template>
+  <GlassCard>
+    <h1 class="text-2xl font-bold">History</h1>
+    <ul class="list-disc ml-9">
+      <li>2022 - : Adways.inc</li>
+      <li>
+          2020 - 2023: Tokyo Institute of Technology, Dept. of
+          Computer Science.<br />
+          Master of Engineering, Artificial Intelligence.<br />
+          Saito Laboratory.
+        </li>
+        <li>
+          2016 - 2021 : Waseda University, School of Fundamental
+          Science and Engineering<br />
+          Bachelor of Engineering, Computer Science and
+          Engineering.<br />
+          Ishikawa Laboratory.
+        </li>
+        <li>2012 - 2016 : Zushi Kaisei Senior High School</li>
+    </ul>
+  </GlassCard>
+</template>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,8 +1,9 @@
 <script setup>
 import GithubButton from "vue-github-button";
 import TwitterButton from "@/components/TwitterButton.vue";
-import GlassCard from "@/components/GlassCard.vue";
+import GlassCard from "@/components/molecules/GlassCard.vue";
 import MainLayout from "@/layouts/MainLayout.vue";
+import HistoryCard from "@/components/organisms/HistoryCard.vue";
 </script>
 
 <template>
@@ -38,26 +39,7 @@ import MainLayout from "@/layouts/MainLayout.vue";
         </div>
       </GlassCard>
     
-      <GlassCard>
-        <h2 class="text-2xl font-bold">History</h2>
-        <ul class="list-disc ml-8">
-          <li>2023 - : Adways.inc</li>
-          <li>
-              2021 - 2023: Tokyo Institute of Technology, Dept. of
-              Computer Science.<br />
-              Master of Engineering, Artificial Intelligence.<br />
-              Saito Laboratory.
-            </li>
-            <li>
-              2017 - 2021 : Waseda University, School of Fundamental
-              Science and Engineering<br />
-              Bachelor of Engineering, Computer Science and
-              Engineering.<br />
-              Ishikawa Laboratory.
-            </li>
-            <li>2013 - 2016 : Zushi Kaisei Senior High School</li>
-        </ul>
-      </GlassCard>
+      <HistoryCard />
     
       <GlassCard>
         <h2 class="text-2xl font-bold">Programming Skills</h2>
